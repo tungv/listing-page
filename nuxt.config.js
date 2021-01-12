8b9eef38c67187f47620b4667684f970dfcf0c38
Hang Tran <ngochang29397@gmail.com>
2021-01-12 23:08:55 +0700

    wip

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
         hid: "og:image",
         property: "og:image",
         content:
-          "/default-thumbnail-social-share.png",
+          "/images/default-thumbnail-social-share.png",
       }
     ],
     link: [
